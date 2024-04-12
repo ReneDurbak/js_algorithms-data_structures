4a1247ef6bd57e57ec93135864869ce4cf0f9019
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-04-12 18:56:04 +0200

    linear search added

---
+++ search_algorithms/linear_search.js
@@ -0,0 +1,33 @@
+//-----------------------
+// my approach
+//-----------------------
+
+// function linear_search(arr, target){
+//     if(arr.includes(target) === false){
+//         return -1
+//     }else{
+//         return arr.indexOf(target)
+//     }
+// }
+
+// Big-O = O(n)
+
+
+
+function linear_search(arr, target){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === target){
+            return i
+        }
+    }
+
+    return -1
+}
+
+// Big-O = O(n)
+
+
+console.log(linear_search([0,1,6], 6))  //2
+console.log(linear_search([10,20,30], 40))  //-1
+console.log(linear_search([4,8,12,16,20], 20))  //4
+
