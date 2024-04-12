1a513ba5951e7ed9663ac0de0a507c66db3a8e07
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-04-12 19:38:58 +0200

    binary search added

---
+++ search_algorithms/binary_search.js
@@ -0,0 +1,27 @@
+function binary_search(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+
+    if(target < arr[middleIndex]){
+        rightIndex = middleIndex - 1
+    }else{
+        leftIndex = middleIndex + 1
+    }
+
+  }
+
+  return -1
+}
+
+console.log(binary_search([-5, 2, 4, 6, 10], 10)); // 4
+console.log(binary_search([-5, 2, 4, 6, 10], 6)); // 3
+console.log(binary_search([-5, 2, 4, 6, 10], 20)); // -1
+
+// Big-O = O(logn)
