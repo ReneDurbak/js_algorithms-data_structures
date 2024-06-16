f7c5d7946663af44de693a0eee45ec17cbe7f423
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-16 10:17:41 +0200

    queue data structure optimised

--- data_structures/queue.js
+++ data_structures/queue.js
@@ -1,43 +1,48 @@
 class Queue {
   constructor() {
-    this.items = [];
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
   }
 
   enqueue(element) {
-    this.items.push(element);
+    this.items[this.rear] = element;
+    this.rear++;
   }
 
   dequeue() {
-    return this.items.shift();
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++
+    return item
   }
 
-  isEmpty() {
-    return this.items.length === 0;
+  isEmpty(){
+    return this.rear - this.front === 0
   }
 
-  peek() {
-    if (!this.isEmpty()) {
-      return this.items[0];
-    }
-    return null;
+  peek(){
+    return this.items[this.front]
   }
 
-  size() {
-    return this.items.length;
+  size(){
+    return this.rear - this.front
   }
 
-  print() {
-    console.log(this.items.toString());
+  print(){
+    console.log(this.items)
   }
 }
 
-const queue = new Queue();
 
+const queue = new Queue()
+
+console.log(queue.isEmpty()) // true
 queue.enqueue(10)
 queue.enqueue(20)
 queue.enqueue(30)
-queue.size()
-queue.print()
-
-console.log(queue.dequeue())
-console.log(queue.peek())
+console.log(queue.size()) // 3
+console.log(queue.peek()) // 10
+queue.print() // { '0': 10, '1': 20, '2': 30 }
+console.log(queue.dequeue()) // 10
+console.log(queue.peek()) // 20
