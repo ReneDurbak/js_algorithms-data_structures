be7ea01321f8f594505508f56029fefd9b7637ce
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-25 22:39:09 +0200

    set added

---
+++ data_structures/set.js
@@ -0,0 +1,13 @@
+const set = new Set([1, 2, 3]);
+set.add(4);
+set.add(2); // ignored because it's duplicate value
+set.delete(3);
+
+console.log(set.has(4));
+console.log(set.size);
+console.log("-------");
+// set.clear()
+
+for (const item of set) {
+  console.log(item);
+}
