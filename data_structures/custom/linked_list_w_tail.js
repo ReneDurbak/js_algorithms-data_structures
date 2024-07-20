8ec50299c79855245ab48c969a716f911a8c0c0b
René Durbák <renedurbak2@gmail.com>
2024-07-20 11:27:21 +0200

    added linked list with tail pointer

---
+++ data_structures/custom/linked_list_w_tail.js
@@ -0,0 +1,70 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Linked_list {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+
+  prepend(value) {}
+
+  append(value) {}
+
+  removeFromFront(index) {}
+
+  removeFromEnd(value) {}
+}
+
+const list = new Linked_list();
+
+//---------------------------------------
+// PREPEND & APPEND
+
+list.prepend(10);
+list.print(); // 10
+
+list.prepend(20);
+// list.append(30);
+list.print(); // 20 10 30
+
+//---------------------------
+// INSERT
+
+// list.insert(10, 0);
+// list.print(); // 10
+// list.insert(20, 0);
+// list.print(); // 20 10
+// list.insert(30, 1);
+// list.print(); // 20 30 10
+// list.insert(40, 2);
+// list.print(); // 20 30 40 10
+
+// console.log(list.getSize());
