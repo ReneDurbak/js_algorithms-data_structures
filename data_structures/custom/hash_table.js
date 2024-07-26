fe4b48aa4e14dbdc973a6a591fdac363e1a24ac5
René Durbák <renedurbak2@gmail.com>
2024-07-26 22:44:57 +0200

    Hash collision with key names that have the same letters solved

--- data_structures/custom/hash_table.js
+++ data_structures/custom/hash_table.js
@@ -15,23 +15,52 @@ class Hash_table {
 
   set(key, value) {
     const index = this.hash(key);
-    this.table[index] = value;
+    // this.table[index] = value;
+    const bucket = this.table[index];
+
+    if (!bucket) {
+      this.table[index] = [[key, value]];
+    } else {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        sameKeyItem[1] = value;
+      } else {
+        bucket.push([key, value]);
+      }
+    }
   }
 
   get(key) {
     const index = this.hash(key);
-    return this.table[index];
+    // return this.table[index];
+    const bucket = this.table[index];
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        return sameKeyItem[1];
+      }
+    }
+    return undefined;
   }
 
   remove(key) {
     const index = this.hash(key);
-    this.table[index] = undefined;
+    // this.table[index] = undefined;
+    const bucket = this.table[index];
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        bucket.splice(bucket.indexOf(sameKeyItem), 1);
+      }
+    }
+
+    return undefined;
   }
 
   display() {
-    for(let i = 0; i < this.table.length; i++) {
-      if(this.table[i]){
-        console.log(i, this.table[i])
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
       }
     }
   }
@@ -39,9 +68,12 @@ class Hash_table {
 
 const table = new Hash_table(50);
 
-table.set("name", "Richard")
-table.set("age", 25)
-table.display() // 1 25 17 Richard 
-console.log(table.get("name")) // Richard
-table.set("mane", "Clark") 
-table.display() // 1 25
+table.set("name", "Richard");
+table.set("age", 25);
+table.display(); // 1 [ [ 'age', 25 ] ] \n 17 [ [ 'name', 'Richard' ] ]
+console.log(table.get("name")); // Richard
+table.set("mane", "Clark");
+table.set("name", "Naruto");
+table.display(); // 1 [ [ 'age', 25 ] ] \n 17 [ [ 'name', 'Naruto' ], [ 'mane', 'Clark' ] ]
+table.remove("name")
+table.display() // 17 [ [ 'mane', 'Clark' ] ]
